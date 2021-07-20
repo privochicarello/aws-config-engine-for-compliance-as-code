1211a6a6f397b444bc7fa0455bb02ddb14c68758
Paul Chicarello <chicarellop@gmail.com>
2021-07-20 19:10:58 -0400

    cleanup and simplify

--- quick-start.sh
+++ quick-start.sh
@@ -1,83 +1,74 @@
 #!/bin/bash
 
-ENV_VAR_FILE=settings.env
+export AWS_PAGER=""
+export AWS_PROFILE="$1"
+export AWS_DEFAULT_REGION="$2"
 
-source $ENV_VAR_FILE
+function usage() {
+  echo -e "Usage: `basename $0` [AWS_PROFILE_NAME] [REGION]\n"
+  echo -e "-h --help             Display this page"
+  exit 0
+}
 
-REGION=$MainRegion
-ACCOUNT_ID=$ComplianceAccountId
-COMPLIANCE_ACCOUNT_SOURCE_BUCKET=compliance-engine-codebuild-source-${ACCOUNT_ID}-${REGION}
-PROFILE=$ComplianceAccountProfileName
-COMPLIANCE_ACCOUNT_CODEPIPELINE_NAME=$CodePipelineComplianceEnginePipelineName
+if [ $# -ne 2 ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    usage
+fi
+
+# Variables dependent on proper arguments
+export ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+export BUCKET_NAME="compliance-engine-codebuild-source-$ACCOUNT_ID-$REGION"
 
 echo "[Deploy rdklib lambda layer]"
 
 NAME_OF_THE_CHANGE_SET=$(aws serverlessrepo create-cloud-formation-change-set \
     --application-id arn:aws:serverlessrepo:ap-southeast-1:711761543063:applications/rdklib \
     --stack-name rdklib-Layer \
-    --region ${REGION} \
-    --profile ${PROFILE} \
     --query "ChangeSetId" | tr -d '\"')
 
-## If there is an existing deployment and no changes, the ChangeSet will come to a failure state and execute-change-set will be failed.
+aws cloudformation wait change-set-create-complete --change-set-name $NAME_OF_THE_CHANGE_SET
 
-aws cloudformation wait change-set-create-complete \
-    --region ${REGION} \
-    --profile ${PROFILE} \
-    --change-set-name $NAME_OF_THE_CHANGE_SET
-
-aws cloudformation execute-change-set \
-    --region ${REGION} \
-    --profile ${PROFILE} \
-    --change-set-name $NAME_OF_THE_CHANGE_SET
+aws cloudformation execute-change-set --change-set-name $NAME_OF_THE_CHANGE_SET
 
 echo "[Deploy Compliance Account]"
 
 aws cloudformation deploy \
     --stack-name compliance-iam \
     --template-file compliance-account-iam.yaml \
-    --parameter-overrides $(cat ${ENV_VAR_FILE}) \
+    --parameter-overrides ComplianceAccountId=$ACCOUNT_ID \
     --no-fail-on-empty-changeset \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --region ${REGION} \
-    --profile ${PROFILE}
+    --capabilities CAPABILITY_NAMED_IAM
 
 aws cloudformation deploy \
     --stack-name compliance-kms \
     --template-file compliance-account-kms-setup.yaml \
-    --parameter-overrides $(cat ${ENV_VAR_FILE}) \
-    --no-fail-on-empty-changeset \
-    --region ${REGION} \
-    --profile ${PROFILE}
+    --no-fail-on-empty-changeset
 
 aws cloudformation deploy \
     --stack-name compliance \
     --template-file compliance-account-initial-setup.yaml \
-    --parameter-overrides $(cat ${ENV_VAR_FILE}) \
-    --no-fail-on-empty-changeset \
-    --region ${REGION} \
-    --profile ${PROFILE}
+    --parameter-overrides ComplianceAccountId=$ACCOUNT_ID ApplicationAccountIds=$ACCOUNT_ID\
+    --no-fail-on-empty-changeset
 
-aws cloudformation deploy \
-    --stack-name compliance-kms \
-    --template-file compliance-account-kms-setup.yaml \
-    --parameter-overrides \
-        GrantComplianceEngineAccess=true \
-        $(cat ${ENV_VAR_FILE}) \
-    --no-fail-on-empty-changeset \
-    --region ${REGION} \
-    --profile ${PROFILE}
+# aws cloudformation deploy \
+#     --stack-name compliance-kms \
+#     --template-file compliance-account-kms-setup.yaml \
+#     --parameter-overrides \
+#         GrantComplianceEngineAccess=true \
+#         $(cat ${ENV_VAR_FILE}) \
+#     --no-fail-on-empty-changeset \
+#     --region ${REGION} \
+#     --profile ${PROFILE}
 
 echo "[Build Rules and deployment scripts]"
 zip ruleset.zip -r rules rulesets-build
-aws s3 cp ruleset.zip s3://${COMPLIANCE_ACCOUNT_SOURCE_BUCKET}/ --region ${REGION} --profile ${PROFILE}
+aws s3 cp ruleset.zip s3://${COMPLIANCE_ACCOUNT_SOURCE_BUCKET}/
 
 echo "[Wait 5 minute for the first codepipeline deployment to complete initialization in compliance account]"
 echo "  - the codepipeline will fail for the first deployment due to application accounts have not yet setup"
 sleep 300
 
-aws cloudformation wait stack-exists --stack-name RDK-Config-Rule-Functions --region ${REGION} --profile ${PROFILE}
-aws cloudformation wait stack-create-complete --stack-name RDK-Config-Rule-Functions --region ${REGION} --profile ${PROFILE}
+aws cloudformation wait stack-exists --stack-name RDK-Config-Rule-Functions
+aws cloudformation wait stack-create-complete --stack-name RDK-Config-Rule-Functions
 
 ## Compliance account can be application account and have rules being deployed.
 echo "[Deploy Application Accounts]"
@@ -85,19 +76,13 @@ echo "[Deploy Application Accounts]"
 aws cloudformation deploy \
     --stack-name application-iam \
     --template-file application-account-iam.yaml \
-    --parameter-overrides $(cat ${ENV_VAR_FILE}) \
     --no-fail-on-empty-changeset \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --region ${REGION} \
-    --profile ${PROFILE}
+    --capabilities CAPABILITY_NAMED_IAM
 
 aws cloudformation deploy \
     --stack-name application \
     --template-file application-account-initial-setup.yaml \
-    --parameter-overrides $(cat ${ENV_VAR_FILE}) \
-    --no-fail-on-empty-changeset \
-    --region ${REGION} \
-    --profile ${PROFILE}
+    --no-fail-on-empty-changeset
 
 echo "[Deploy rules with CodePipeline]"
-aws codepipeline start-pipeline-execution --name ${COMPLIANCE_ACCOUNT_CODEPIPELINE_NAME} --region ${REGION} --profile ${PROFILE}
+aws codepipeline start-pipeline-execution --name ${BUCKET_NAME}
